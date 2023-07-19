c80fe0dbf271bd8affa7b07d927e90fad6d4e804
Nimsara3838383 <112757253+Nimsara3838383@users.noreply.github.com>
2023-07-20 02:25:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['94719945674', 'Nimesh', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
